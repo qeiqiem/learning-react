29b4bd48333e4b564eda27d6756ee239b499a515
Gakyeong <qeiqiem@gmail.com>
2021-11-11 00:51:19 +0900

    [movie] useEffect

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -1,11 +1,37 @@
 import React from "react";
-import Button from "./Button";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [counter, setCounter] = useState(0);
+    const [keyword, setKeyword] = useState("");
+    const onClick = () => setCounter((cur) => cur + 1);
+    const onChange = (e) => setKeyword(e.target.value);
+
+    useEffect(() => {
+        console.log("once");
+    }, []);
+
+    useEffect(() => {
+        console.log("key");
+    }, [keyword]);
+
+    useEffect(() => {
+        console.log("click");
+    }, [counter]);
+
+    useEffect(() => {
+        console.log("keyword or click");
+    }, [keyword, counter]); // 둘 중 하나가 실행될 떄
+
     return (
-        <div className="App">
-            <h1>Hallo</h1>
-            <Button text={"hello"} />
+        <div>
+            <input
+                value={keyword}
+                onChange={onChange}
+                type="text"
+                placeholder="search..."
+            />
+            <button onClick={onClick}>click</button>
         </div>
     );
 }
