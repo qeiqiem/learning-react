26b92c6f07ea35f83dcd38bb05889f57ec4e9838
Gakyeong <qeiqiem@gmail.com>
2021-11-12 00:35:29 +0900

    [movie] data.map()

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -5,10 +5,15 @@ function App() {
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
     const getMovies = async () => {
-        const response = await fetch(
-            "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-        );
-        const json = await response.json();
+        // const response = await fetch(
+        //     "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+        // );
+        // const json = await response.json();
+        const json = await (
+            await fetch(
+                "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+            )
+        ).json();
         setMovies(json.data.movies);
         setLoading(false);
     };
@@ -18,7 +23,30 @@ function App() {
     }, []);
 
     console.log(movies);
-    return <div>{loading ? <h1>Loading....</h1> : null}</div>;
+    return (
+        <div>
+            {loading ? (
+                <h1>Loading....</h1>
+            ) : (
+                movies.map((movie) => (
+                    <div>
+                        <h2>{movie.title_long}</h2>
+                        <img src={movie.medium_cover_image} />
+                        <p>
+                            <strong>Jenres </strong>|
+                            {movie.genres.map((g) => (
+                                <span key={g}> | {g}</span>
+                            ))}
+                        </p>
+                        <ul>
+                            <li>{movie.runtime}mins</li>
+                            <li>{movie.summary}</li>
+                        </ul>
+                    </div>
+                ))
+            )}
+        </div>
+    );
 }
 
 export default App;
