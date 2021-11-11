79cadb3849ae4f18d6fde86ac638fa0edeb8ffed
Gakyeong <qeiqiem@gmail.com>
2021-11-11 23:59:39 +0900

    [movie] async, await

--- movie-app/src/App.js
+++ movie-app/src/App.js
@@ -4,17 +4,19 @@ import { useState, useEffect } from "react";
 function App() {
     const [loading, setLoading] = useState(true);
     const [movies, setMovies] = useState([]);
+    const getMovies = async () => {
+        const response = await fetch(
+            "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
+        );
+        const json = await response.json();
+        setMovies(json.data.movies);
+        setLoading(false);
+    };
 
     useEffect(() => {
-        fetch(
-            "https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year"
-        )
-            .then((response) => response.json())
-            .then((json) => {
-                setMovies(json.data.movies);
-                setLoading(false);
-            });
+        getMovies();
     }, []);
+
     console.log(movies);
     return <div>{loading ? <h1>Loading....</h1> : null}</div>;
 }
