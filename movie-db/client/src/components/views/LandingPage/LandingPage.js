cc063011906d259cfc25d84ba2cc22bdbe53e207
Gakyeong <qeiqiem@gmail.com>
2021-11-18 15:49:21 +0900

    [movie-db] Landing Pages.. working..

--- movie-db/client/src/components/views/LandingPage/LandingPage.js
+++ movie-db/client/src/components/views/LandingPage/LandingPage.js
@@ -1,46 +1,50 @@
+// Defaults
 import React, { useState, useEffect } from "react";
 import { API_URL, IMAGE_BASE_URL } from "../../Config";
 import { API_KEY } from "../../ConfigKey";
+// Components
+import MainImage from "./Section/MainImage";
 
 function LandingPage() {
     const [movies, setMovies] = useState([]);
     const [mainMovie, setMainMovie] = useState(null);
 
-    const getMainMovie = async () => {
-        // 원래코드..
-        // fetch(
-        //     `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-        // )
-        //     .then((res) => res.json())
-        //     .then((res) => console.log(res));
-
-        const json = await (
-            await fetch(
-                `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`
-            )
-        ).json();
-        console.log(json);
-        // setMainMovie(json.)
-    };
-
     useEffect(() => {
         const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
-        getMainMovie();
+        fetchMovies(endPoint);
     }, []);
+
+    const fetchMovies = async (endPoint) => {
+        //response 데이터 받아옴
+        /* fetch(endPoint)
+            .then((res) => res.json())
+            .then((res) => {
+                console.log(res);
+                setMovies(res.results);
+            }); */
+        const json = await (await fetch(endPoint)).json();
+        setMovies(json.results);
+        setMainMovie(json.results[0]);
+    };
 
     return (
         <div style={{ width: "100%", margin: "0" }}>
             {/* Main Image */}
-            {/* {MainMovieImage && (
+            {mainMovie && (
                 <MainImage
-                    image={`${IMAGE_BASE_URL}w1280${MainMovieImage.backdrop_path}`}
-                /> */}
+                    image={`${IMAGE_BASE_URL}w1280${mainMovie.backdrop_path}`}
+                    title={mainMovie.original_title}
+                    text={mainMovie.overview}
+                />
+            )}
+
             <div style={{ width: "85%", margin: "1rem auto" }}>
                 <h2>Movies by latest</h2>
                 <hr />
 
                 {/* Movie Grid Cards */}
             </div>
+
             <div
                 style={{ display: "flex", justifyContent: "center" }}
             >
