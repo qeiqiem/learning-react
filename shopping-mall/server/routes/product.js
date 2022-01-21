04b7d47c6f966d13183fd416926dd04c9373c29d
Gakyeong <qeiqiem@gmail.com>
2022-01-21 19:47:46 +0900

    FEAT: production 날라간거 추가..눈물남..오타..fix..

--- shopping-mall/server/routes/product.js
+++ shopping-mall/server/routes/product.js
@@ -3,11 +3,27 @@
 그렇게 되면 index.js (최초 실행시) 너무 많은 코드를 실행해야 함
 ==> route폴더에서 분기하여 처리
 (user.js와 같은 로직으로 처리)
-
 */
-
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+
+//=================================
+//             Product
+//=================================
+
+var storage = multer.diskStorage({
+    //  파일 저장 경로
+    destination: (req, file, cb) => {
+        cb(null, "uploads/");
+    },
+    //  파일이름
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`);
+    },
+});
+
+var upload = multer({ storage: storage }).single("file");
 
 //=================================
 //             Product
@@ -15,17 +31,31 @@ const router = express.Router();
 
 /* 
 이미 index.js에서 아래 url을 타고 왔기 때문에 
-
-FileUpload.js처럼 루트를 모두 적을 필요없이
-   axios
-        .post("api/product/image", formData, config)
-
-
-/image 루트만 남겨 놓으면 된다. + request handler..
-        */
-router.post("/image", (req, ress) => {
+/image 루트만 남겨 놓으면 된다. 
+ */
+router.post("/image", (req, res) => {
     // 가져온 이미지를 저장 -> multer 라이브러리 사용
     // multer는 서버 dependency기 때문에 client단이 아닌 root에서 받아야 한다. npm install multer --save
+
+    /* 
+    multer 모듈에 따른 로직으로 이름 경로가 정해진다.
+    ex) res > req > file > path .. 이런 경로로 해당 정보들이 저장되어 있고,
+    path를 가져오기 위해서 res.req.file.path 이렇게 접근한 것
+    */
+    upload(req, res, (err) => {
+        if (err) {
+            // 에러발생
+            return res.json({ success: false, err });
+        }
+        return res.json({
+            success: true,
+            // 성공 -> 파일정보전달
+            filePath: res.req.file.path,
+            fileName: res.req.file.filename,
+        });
+    });
+
+    //     이렇게 back -> front 로 정보 전달
 });
 
 module.exports = router;
